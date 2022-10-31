0cabf71e170e8574aefca232546cb8b6ffecb283
Kyle Bolton <hello@kylebolton.me>
2022-10-31 16:20:56 +0000

    Initial package adds + removals + changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,15 @@
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import { getAllPokemons, getPokemon } from '../src/services/pokemon.services';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
-interface Pokemon {
-  id: number;
-  name: string;
-}
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
-const App = () => {
-  const [pokemon, setPokemon] = useState<Pokemon>();
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-
-  const loadAllPokemons = async () => {
-    const pokemons = await getAllPokemons();
-    setPokemons(pokemons);
-  }
-
-  useEffect(() => {
-    loadAllPokemons();
-  }, []);
-
-  console.log(pokemons);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+)
 
 export default App;
